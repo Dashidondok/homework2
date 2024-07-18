c7ece87e37c348a5aa3cc43e234c5161c082b86b
Dashidondok <dashidondok22@mail.ru>
2024-07-18 21:47:15 +0900

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,7 @@
+first_name = (12)
+apple_count = (1.5)
+orange_count = (apple_count/first_name)
+first_name = '12'
+apple_count = '1.5'
+second_name = 'Python'
+print('Курс:',second_name,'всего задач:',first_name,'затрачено часов:',apple_count,'среднее время выполнения:',orange_count,'часа.')
